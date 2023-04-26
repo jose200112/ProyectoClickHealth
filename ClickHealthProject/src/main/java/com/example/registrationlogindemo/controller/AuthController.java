74bc25e3e6773dc5688f011428dbda6268f9f09d
josemunozmunoz@iessoterohernandez.es <josemunozmunoz@iessoterohernandez.es>
2023-04-26 02:02:18 +0200

    Septima version

--- ClickHealthProject/src/main/java/com/example/registrationlogindemo/controller/AuthController.java
+++ ClickHealthProject/src/main/java/com/example/registrationlogindemo/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.example.registrationlogindemo.controller;
 
-import com.clickhealth.object.Columna;
 import com.example.registrationlogindemo.dto.UserDto;
 import com.example.registrationlogindemo.entity.User;
 import com.example.registrationlogindemo.service.UserService;
@@ -8,7 +7,6 @@ import jakarta.validation.Valid;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collection;
 import java.util.List;
 
 @Controller
@@ -28,7 +25,21 @@ public class AuthController {
     public AuthController(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
     
+    @GetMapping("/Inicio")
+    public String inicio() {
+    	return "Inicio";
+    }
+    
+    @GetMapping("hello")
+    public String hello() {
+    	return "hello";
+    }
 
     @GetMapping("/login")
     public String loginForm() {
@@ -38,9 +49,27 @@ public class AuthController {
         } else {
         	
         }
-    	return "redirect:/redirige";
+    	return "redirect:/";
+    }
+
+    // handler method to handle user registration request
+    @GetMapping("register")
+    public String showRegistrationForm(Model model){
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "register";
     }
     
+//    // handler method to handle user registration request
+//    @GetMapping("registerUser")
+//    public String showRegistrationUser(Model model){
+//        UserDto user = new UserDto();
+//        model.addAttribute("user", user);
+//        return "registerUser";
+//    }
+    
+    
+
     // handler method to handle register user form submit request
     @PostMapping("/register/save")
     public String registration(@Valid @ModelAttribute("user") UserDto user,
@@ -58,40 +87,31 @@ public class AuthController {
         return "redirect:/register?success";
     }
     
-    // handler method to handle user registration request
-    @GetMapping("register")
-    public String showRegistrationForm(Model model){
-        UserDto user = new UserDto();
-        model.addAttribute("user", user);
-        return "register";
+//    @PostMapping("/register/saveUser")
+//    public String userRegistration(@Valid @ModelAttribute("user") UserDto user,
+//                               BindingResult result,
+//                               Model model){
+//        User existing = userService.findByEmail(user.getEmail());
+//        if (existing != null) {
+//            result.rejectValue("email", null, "There is already an account registered with that email");
+//        }
+//        if (result.hasErrors()) {
+//            model.addAttribute("user", user);
+//            return "register";
+//        }
+//        userService.saveUser(user);
+//        return "redirect:/register?success";
+//    }
+
+    @GetMapping("Enfermero/users")
+    public String listRegisteredUsers(Model model){
+        List<UserDto> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "users";
     }
     
     
-    @GetMapping("/redirige")
-    public String redirigeRol() {
-    	String auth ="";
-        Collection<? extends GrantedAuthority> authority = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
-        for(GrantedAuthority author: authority) {
-        	auth  = author.toString();
-        }
-        if(auth.equals("ROLE_ADMIN")) {
-            return "redirect:/admin/inicioAdmin";
-        }
-        
-        if(auth.equals("ROLE_ENFERMERO")) {
-        	return "redirect:/enfermero/inicioEnfermero";
-        }
-        
-        if(auth.equals("ROLE_USUARIO")) {
-        	return "redirect:/usuario/inicioUsuario";
-        }
-        
-        if(auth.equals("ROLE_MEDICO")) {
-        	return "redirect:/medico/inicioMedico";
-        }
-                
-		return "redirect:/login";
-    }
+    
     
     
 }
