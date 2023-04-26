a11343c6ee503e3ebf2b9120c0153f9bda75d24a
josemunozmunoz@iessoterohernandez.es <josemunozmunoz@iessoterohernandez.es>
2023-04-26 09:52:18 +0200

    1.4 version

--- ClickHealthProject/src/main/java/com/example/registrationlogindemo/controller/AuthController.java
+++ ClickHealthProject/src/main/java/com/example/registrationlogindemo/controller/AuthController.java
@@ -41,31 +41,6 @@ public class AuthController {
     	return "redirect:/redirige";
     }
     
-    @GetMapping("register")
-    public String showRegistrationForm(Model model){
-        UserDto user = new UserDto();
-        model.addAttribute("user", user);
-        return "register";
-    }
-    
-
-    // handler method to handle register user form submit request
-    @PostMapping("/register/save")
-    public String registration(@Valid @ModelAttribute("user") UserDto user,
-                               BindingResult result,
-                               Model model){
-        User existing = userService.findByEmail(user.getEmail());
-        if (existing != null) {
-            result.rejectValue("email", null, "There is already an account registered with that email");
-        }
-        if (result.hasErrors()) {
-            model.addAttribute("user", user);
-            return "register";
-        }
-        userService.saveUser(user);
-        return "redirect:/register?success";
-    }
-    
     
     @GetMapping("/redirige")
     public String redirigeRol() {
